27d83c1b05bf946a1b0cd7aa9f4878ca8cd6aa97
URIT <urit@2a02-8428-27d6-7901-86e6-98df-c06e-1835.rev.sfr.net>
2023-02-23 15:29:21 +0100

    setup declaration 4

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,21 +1,19 @@
 <template>
-  <client-only>
     <h1>TESTAMENT</h1>
     <h2>An interactive story</h2>
     <p>{{intro}}</p>
     <button @click="erase()">Effacer</button>
-  </client-only>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-  const intro = ref("Salut les kids")
-  
-  // functions that mutate state and trigger updates
-  const erase = () => {
-    intro.value = "erased";
-  }
+const intro = ref("Salut les kids")
+
+// functions that mutate state and trigger updates
+const erase = () => {
+  intro.value = "erased";
+}
 
 </script>
 
