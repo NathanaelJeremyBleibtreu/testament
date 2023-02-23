59b983a103d0ac0ffc58fd8e3f933df09f05abbb
URIT <urit@2a02-8428-27d6-7901-86e6-98df-c06e-1835.rev.sfr.net>
2023-02-23 14:44:29 +0100

    test vue

---
+++ components/Welcome.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>TESTAMENT</h1>
+  <h2>An interactive story</h2>
+  <p>{{intro}}</p>
+  <button @click="erase()">Effacer</button>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const intro = ref("Salut les kids")
+
+// functions that mutate state and trigger updates
+function erase() {
+  intro.value = "erased";
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial intro is ${intro.value}.`)
+})
+</script>
+
+
+<style>
+
+</style>
